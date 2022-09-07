a82fa3d3ba1e260e58d35b1e4cdcc572a12b941a
iHoHyeon <hyunee169@naver.com>
2022-09-07 22:57:26 +0900

    💄 [Design] BodyText 컴포넌트 & 스토리 작성

---
+++ src/components/BodyText.tsx
@@ -0,0 +1,10 @@
+import { HTMLAttributes } from 'react';
+
+interface Props extends HTMLAttributes<HTMLSpanElement> {}
+
+const BodyText = (props: Props) => {
+  const { children, className } = props;
+  return <span className={className + ' ' + 'font-normal '}>{children}</span>;
+};
+
+export default BodyText;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h1 className=" leading-[34px] text-[28px] font-bold font-montserrat">Montserrat</h1>;
+  return <h1 className="font-bold  font-montserrat">Montserrat</h1>;
 };
 
 export default Home;
