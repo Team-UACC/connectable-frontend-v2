e39c8cfa59cab124aace89067429447ee1e07350
iHoHyeon <hyunee169@naver.com>
2022-09-07 22:39:09 +0900

    💄 [Design] 버튼 컴포넌트 & 스토리 작성

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,8 @@
+import '../src/styles/globals.css';
+import '../public/fonts/SUIT-woff2/SUIT.css';
+import '../public/fonts/GmarketSansTTF/GmarketSansTTF.css';
+import '../public/fonts/Montserrat/Montserrat.css';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import { ButtonHTMLAttributes, forwardRef, Ref } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color: 'black' | 'white';
+  fullWidth?: boolean;
+}
+
+const Button = forwardRef(function Button(props: Props, forwardRef: Ref<HTMLButtonElement>) {
+  const { fullWidth = true, color, children, ...rest } = props;
+
+  const widthClassName = (fullWidth ? `w-full` : `w-auto px-4`) + ' ';
+  const colorClassName =
+    (color === 'black'
+      ? 'bg-black text-white disabled:bg-gray2 disabled:text-gray4'
+      : 'bg-white text-black border-black border-[1px] disabled:bg-background1 disabled:text-gray5 disabled:border-gray6') +
+    ' ';
+
+  return (
+    <button
+      ref={forwardRef}
+      className={widthClassName + colorClassName + 'rounded-[0.4rem] leading-[1.4rem] h-[3.5rem] '}
+      {...rest}
+    >
+      <span>{children}</span>
+    </button>
+  );
+});
+
+export default Button;
