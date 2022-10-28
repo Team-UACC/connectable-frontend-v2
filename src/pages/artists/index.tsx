d499cfe2f50d386892c76812a10cd2a788198592
iHoHyeon <hyunee169@naver.com>
2022-10-28 19:00:46 +0900

    💄 [Design] Artist 페이지 UI 작성

---
+++ src/pages/artists/index.tsx
@@ -0,0 +1,40 @@
+import { ReactElement } from 'react';
+
+import { fetchAllArtists } from '~/apis/artists';
+import ArtistCardList from '~/components/Artist/ArtistCardList';
+import Label from '~/components/Design/Label';
+import Layout from '~/components/Layout';
+import { Artist } from '~/types/artistType';
+
+export async function getStaticProps() {
+  const artistsList = await fetchAllArtists();
+
+  return {
+    props: { artistsList },
+  };
+}
+
+interface Props {
+  artistsList: Array<Artist>;
+}
+
+const ArtistPage = ({ artistsList }: Props) => {
+  return (
+    <div className="px-[18px] py-[40px] flex flex-col items-center">
+      <Label title="ARTISTS" color="blue" />
+      <div className="mt-4 text-sm leading-6 text-center text-gray4">
+        오직 커넥터블에서만!
+        <br />
+        당신의 아티스트를 만나보세요
+      </div>
+      <ArtistCardList artistsList={artistsList} />
+    </div>
+  );
+};
+
+ArtistPage.getLayout = (page: ReactElement) => (
+  <Layout headerType="home" selectedFooter="artists" bgColor="black">
+    {page}
+  </Layout>
+);
+export default ArtistPage;
---
+++ src/types/artistType.ts
@@ -0,0 +1,4 @@
+export interface Artist {
+  artistImage: string;
+  artistName: string;
+}
