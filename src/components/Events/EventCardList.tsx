d5212df4d7424800ade86e201ef1e9e1111a0c6a
iHoHyeon <hyunee169@naver.com>
2022-11-07 18:31:31 +0900

    ♻️ [Refactor] EventCardList 컴포넌트 화

---
+++ src/components/Events/EventCardList.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import { useMemo } from 'react';
+
+import { EventSimpleType } from '~/types/eventType';
+
+import EventCard from './EventCard';
+
+const EventCardList = ({ events }: { events: Array<EventSimpleType> }) => {
+  const length = useMemo(() => events.length, [events]);
+
+  return (
+    <ul className="flex flex-wrap w-full justify-evenly ">
+      {events.map(({ id, name, description, image, salesTo }) => (
+        <Link href={`/events/${id}`} key={id}>
+          <a className=" basis-[45%] mt-8 ">
+            <EventCard
+              title={name}
+              description={description}
+              image={image}
+              saleStatus={salesTo > new Date().getTime() ? '판매중' : '판매종료'}
+              overlap={false}
+            />
+          </a>
+        </Link>
+      ))}
+      {length % 2 === 1 && <div className=" basis-[45%] mt-8 " />}
+    </ul>
+  );
+};
+
+export default EventCardList;
