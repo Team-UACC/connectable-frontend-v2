7772502bfb9df2befdc265bef4706e6b82604c14
iHoHyeon <hyunee169@naver.com>
2022-11-24 16:33:21 +0900

    💄 [Design] Skeleton UI 컴포넌트 & 스토리 추가

--- src/components/Tickets/TicketCard/TicketSkeleton.tsx
+++ src/components/Tickets/TicketCard/TicketSkeleton.tsx
@@ -1,16 +1,15 @@
+import SkeletonBox from '~/components/Design/SkeletonBox';
+
 const TicketSkeleton = () => {
   return (
-    <div
-      role="status"
-      className="flex items-center justify-between w-full px-8 py-4 bg-white max-w-layout animate-pulse"
-    >
-      <div className="flex items-center justify-center min-w-[100px] h-[100px] bg-gray6 rounded"></div>
+    <div role="status" className="flex items-center justify-between w-full px-8 py-4 bg-white max-w-layout">
+      <SkeletonBox width={100} height={100} className="mb-2.5" />
       <div className="w-2/3">
-        <div className="h-2 bg-gray6 rounded-full w-[50px] mb-2.5"></div>
-        <div className="h-2 bg-gray6 rounded-full mb-2.5"></div>
-        <div className="h-2 bg-gray6 rounded-full w-[150px] mb-2.5"></div>
-        <div className="h-2 bg-gray6 rounded-full w-[100px] mb-2.5"></div>
-        <div className="h-2 bg-gray6 rounded-full w-[120px]"></div>
+        <SkeletonBox width={50} height={8} className="mb-2.5" />
+        <SkeletonBox width={150} height={8} className="mb-2.5" />
+        <SkeletonBox width={150} height={8} className="mb-2.5" />
+        <SkeletonBox width={100} height={8} className="mb-2.5" />
+        <SkeletonBox width={120} height={8} className="mb-2.5" />
       </div>
       <span className="sr-only">Loading...</span>
     </div>
